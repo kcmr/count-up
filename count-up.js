5805aee94f596bd39ff34c3c263456ac149b908c
Kus Cámara <kcmr@users.noreply.github.com>
2017-06-18 12:23:11 +0200

    refactor(js): force to evaluate decimals

--- count-up.js
+++ count-up.js
@@ -14,9 +14,12 @@
         observer: '_valueChanged'
       },
 
+      _previousValue: Number,
+
       decimals: {
         type: Number,
-        value: 0
+        value: 0,
+        observer: '_updateCountUp'
       },
 
       duration: {
@@ -34,38 +37,54 @@
         value: false
       },
 
-      separator: String,
-      decimal: String
+      separator: {
+        type: String,
+        value: ','
+      },
+
+      decimal: {
+        type: String,
+        value: '.'
+      },
+
+      _countup: {
+        type: Function,
+        computed: '_setCountUp(value, _previousValue, decimals, duration)'
+      }
     },
 
-    _countup: function(from, to) {
-      return new CountUp(this.$.value, from, to, this.decimals, this.duration, {
+    _setCountUp: function(value, previousValue, decimals, duration) {
+      return new CountUp(this.$.value, Number(previousValue), Number(value), decimals, duration, {
         useEasing: !this.noEasing,
         useGrouping: !this.noGrouping,
         separator: this.separator,
-        decimals: this.decimal
+        decimal: this.decimal
       });
     },
 
     _valueChanged: function(value, previousValue) {
-      if (previousValue === undefined) {
-        previousValue = 0;
-      }
-
-      this.countup = this._countup(Number(previousValue), Number(value));
+      this._previousValue = previousValue === undefined ? 0 : previousValue;
       this.start();
     },
 
     start: function(cb) {
-      this.countup.start(cb);
+      this._countup.start(cb);
     },
 
     reset: function() {
-      this.countup.reset();
+      this._countup.reset();
     },
 
     update: function(value) {
-      this.countup.update(value);
+      this._countup.update(value);
+    },
+
+    _updateCountUp: function(value, previousValue) {
+      if (previousValue !== undefined) {
+        setTimeout(() => {
+          this.update(this.value); // reevaluate decimals
+        }, 1);
+      }
     }
   });
 
